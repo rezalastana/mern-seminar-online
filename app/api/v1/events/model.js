835f021c11f96a41c0fb435cd9bba32cf619ac83
rezalastana <rezalastana.dev@gmail.com>
2024-09-08 19:20:31 +0700

    feat: model events && create event mehtod

---
+++ app/api/v1/events/model.js
@@ -0,0 +1,86 @@
+const mongoose = require("mongoose");
+
+// ada 2 buath schema dalam 1 file kenapa tidak dipisah?
+// karena ticketCategoriesSchema hanya digunakan pada eventSchema
+//  ticketCategories schema
+const ticketCategoriesSchema = new mongoose.Schema({
+    type: {
+        type: String,
+        required: [true, "Type tiket harus diisi"],
+    },
+    price: {
+        type: Number,
+        default: 0,
+    },
+    stock: {
+        type: Number,
+        default: 0,
+    },
+    statusTicketCategory: {
+        type: Boolean,
+        enum: [true, false],
+        default: true,
+    },
+    expired: {
+        type: Date,
+    },
+});
+
+// events schema
+const eventSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: [true, "Judul harus diisi"],
+            minLength: [3, "Panjang title minimal 3 karakter"],
+            maxLength: [50, "Panjang title maksimal 50 karakter"],
+        },
+        date: {
+            type: Date,
+            required: [true, "Tanggal dan waktu harus diisi"],
+        },
+        about: {
+            type: String,
+        },
+        tagline: {
+            type: String,
+            required: [true, "Tagline harus diisi"],
+        },
+        keypoint: {
+            type: [String],
+        },
+        venueName: {
+            type: String,
+            required: [true, "Nama tempat harus diisi"],
+        },
+        statusEvent: {
+            type: Boolean,
+            enum: ["Draft", "Published"],
+            default: "Draft",
+        },
+        tickets: {
+            // karena mungkin ada beberapa jenis tiket, maka dibuat array
+            type: [ticketCategoriesSchema],
+            required: [true],
+        },
+        image: {
+            // jika hanya ingin menampilkan 1 data saja, maka menggunakan obh
+            type: mongoose.Types.ObjectId,
+            ref: "Image",
+            required: true,
+        },
+        category: {
+            type: mongoose.Types.ObjectId,
+            ref: "Categories",
+            required: true,
+        },
+        talent: {
+            type: mongoose.Types.ObjectId,
+            ref: "Talents",
+            required: true,
+        },
+    },
+    { timestamps: true }
+);
+
+module.exports = mongoose.model("Events", eventSchema);
--- app/services/mongoose/events.js
+++ app/services/mongoose/events.js
@@ -4,6 +4,7 @@ const Events = require("../../api/v1/events/model");
 //checking
 const { checkingCategories } = require("./categories");
 const { checkingImage } = require("./images");
+const { checkingTalents } = require("./talents");
 //error
 const { NotFoundError, BadRequestError } = require("../../errors");
 const { path } = require("express/lib/application");
@@ -46,6 +47,51 @@ const getAllEvents = async (req) => {
     return result;
 };
 
+const createEvents = async (req) => {
+    const {
+        title,
+        date,
+        about,
+        tagline,
+        venueName,
+        keyPoint,
+        statusEvent,
+        tickets,
+        image,
+        category,
+        talent,
+    } = req.body;
+
+    // cari image, category, dan talents dengan field id
+    await checkingImage(image);
+    await checkingCategories(category);
+    await checkingTalents(talent);
+
+    // cari events dengan field title, agar tidak ada nama yang sama
+    const check = await Events.findOne({ title });
+
+    // apabila nama sudah ada maka akan muncul error
+    if (check) throw new BadRequestError(`Event ${title} sudah ada`);
+
+    //jika tidak ada duplicat, maka create events
+    const result = await Events.create({
+        title,
+        date,
+        about,
+        tagline,
+        venueName,
+        keyPoint,
+        statusEvent,
+        tickets,
+        image,
+        category,
+        talents: talent,
+    });
+
+    return result;
+};
+
 module.exports = {
     getAllEvents,
+    createEvents,
 };
